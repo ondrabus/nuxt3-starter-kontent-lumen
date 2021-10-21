d258f127d7515e4b2c830a6430b0dd43a00964d0
Ondřej Polesný <polesny@email.cz>
2021-10-21 17:08:36 +0200

    Adjusts Kontent plugin to add a tracking header

--- src/plugins/kontent.ts
+++ src/plugins/kontent.ts
@@ -2,12 +2,24 @@ import { defineNuxtPlugin } from '#app'
 import { DeliveryClient, IDeliveryClientConfig } from '@kentico/kontent-delivery'
 
 export default defineNuxtPlugin(nuxtApp => {
-    const config = nuxtApp.$config.kontent as IDeliveryClientConfig
+
+    const config = JSON.parse(JSON.stringify(nuxtApp.$config.kontent)) as IDeliveryClientConfig
 
     if (!config){
         console.error("Nuxt 3 Kontent plugin: configuration object is either not present or does not fit the config format.")
         return
     }
+
+    if (!config.defaultQueryConfig){
+        config.defaultQueryConfig = { customHeaders: [] }
+    }
+    if (!config.defaultQueryConfig.customHeaders){
+        config.defaultQueryConfig.customHeaders = [];
+    }
+    config.defaultQueryConfig.customHeaders.push({
+        header: 'X-KC-SOURCE',
+        value: 'nuxt3'
+    })
 
     const kontent = new DeliveryClient(config)
     nuxtApp.provide('kontent', kontent)
